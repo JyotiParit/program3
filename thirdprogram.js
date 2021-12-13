41dbf14d11bd0eb39be1e0ea8b41daa7217a6337
JyotiParit <jyotiparit88@gmail.com>
2021-12-13 17:09:46 +0530

    sixth commit

--- thirdprogram.js
+++ thirdprogram.js
@@ -3,11 +3,13 @@ var person={
     firstName:"Tejashree",
     lastName:"Parit",
     age:"08",
+    gender:"Female",
     fullName:function()
     {
         console.log(this.firstName+" "+this.lastName);
         console.log(person.firstName+" "+person.lastName);
-        console.log(this.age+""); 
+        console.log(this.age+"");
+        console.log(this.gender+""); 
     } 
 
 }
